4e155b73122202825e62bd548f38e8a368dff8de
Cesar Jeanroy <cesar.jeanroy@gmail.com>
2018-03-22 21:52:42 -0400

    Home page ok

--- angular-client/src/app/shared/components/chart/chart.component.ts
+++ angular-client/src/app/shared/components/chart/chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-chart',
@@ -12,26 +12,6 @@ export class ChartComponent {
   @Input('options') lineChartOptions:any = {
     responsive: true
   };
-  @Input('legend') lineChartLegend:boolean = true;
-  @Input('chartType') lineChartType:string = 'line';
-
-    public randomize():void {
-      let _lineChartData:Array<any> = new Array(this.lineChartData.length);
-      for (let i = 0; i < this.lineChartData.length; i++) {
-        _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
-        for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-          _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
-        }
-      }
-      this.lineChartData = _lineChartData;
-    }
-   
-    // events
-    public chartClicked(e:any):void {
-      console.log(e);
-    }
-   
-    public chartHovered(e:any):void {
-      console.log(e);
-    }
+  @Input('legend') lineChartLegend:boolean = true
+  @Input('chartType') lineChartType:string = 'line'
 }
