2c0bfbe41b3de7293ce24a568367326b0efc003b
Cesar Jeanroy <cesar.jeanroy@gmail.com>
2018-03-22 20:32:58 -0400

    chart + switch

---
+++ angular-client/src/app/shared/components/chart/chart.component.ts
@@ -0,0 +1,37 @@
+import { Component, Input, OnChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-chart',
+  templateUrl: './chart.component.html',
+  styleUrls: ['./chart.component.css']
+})
+export class ChartComponent {
+
+  @Input('datasets') lineChartData:Array<any>
+  @Input('labels') lineChartLabels:Array<any>
+  @Input('options') lineChartOptions:any = {
+    responsive: true
+  };
+  @Input('legend') lineChartLegend:boolean = true;
+  @Input('chartType') lineChartType:string = 'line';
+
+    public randomize():void {
+      let _lineChartData:Array<any> = new Array(this.lineChartData.length);
+      for (let i = 0; i < this.lineChartData.length; i++) {
+        _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
+        for (let j = 0; j < this.lineChartData[i].data.length; j++) {
+          _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
+        }
+      }
+      this.lineChartData = _lineChartData;
+    }
+   
+    // events
+    public chartClicked(e:any):void {
+      console.log(e);
+    }
+   
+    public chartHovered(e:any):void {
+      console.log(e);
+    }
+}
